a9d3616c6df547906946bffd812143a4a7aefc44
vishnuxx <vishnuvs2002it@gmail.com>
2022-01-09 13:22:16 +0530

    added route activity

---
+++ app/src/main/java/com/ideologics/BusTopper/RouteActivity.kt
@@ -0,0 +1,11 @@
+package com.ideologics.BusTopper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RouteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_route)
+    }
+}
